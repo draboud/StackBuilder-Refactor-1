dfc4fdb62fc6e19f78d3cb63623b1f8447f11802
dean <dean@beamdot3d.com>
2025-05-13 10:55:18 -0600

    basic build by file

--- builder.js
+++ builder.js
@@ -1,43 +1,43 @@
-// require("esbuild")
-//   .build({
-//     entryPoints: ["src/js/views"],
-//     bundle: true,
-//     minify: true,
-//     sourcemap: true,
-//     outfile: "dist/controller.js",
-//   })
-//   .catch(() => process.exit(1));
+require("esbuild")
+  .build({
+    entryPoints: ["src/js/controller.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: "dist/controller.js",
+  })
+  .catch(() => process.exit(1));
 
-const esbuild = require("esbuild");
-const fs = require("fs");
-const path = require("path");
+// const esbuild = require("esbuild");
+// const fs = require("fs");
+// const path = require("path");
 
-async function build() {
-  const entryDir = "./src/js"; // Replace with your directory path
-  const outDir = "./dist";
+// async function build() {
+//   const entryDir = "./src/js"; // Replace with your directory path
+//   const outDir = "./dist";
 
-  const entryPoints = fs
-    .readdirSync(entryDir)
-    .filter((file) => file.endsWith(".js"))
-    .reduce((acc, file) => {
-      const name = path.parse(file).name;
-      acc[name] = path.resolve(entryDir, file);
-      return acc;
-    }, {});
+//   const entryPoints = fs
+//     .readdirSync(entryDir)
+//     .filter((file) => file.endsWith(".js"))
+//     .reduce((acc, file) => {
+//       const name = path.parse(file).name;
+//       acc[name] = path.resolve(entryDir, file);
+//       return acc;
+//     }, {});
 
-  try {
-    await esbuild.build({
-      entryPoints: ["src/js/controller.js"],
-      outdir: outDir,
-      bundle: true,
-      minify: true,
-      platform: "browser",
-      format: "esm",
-      splitting: true,
-    });
-    console.log("Build successful!");
-  } catch (error) {
-    console.error("Build failed:", error);
-  }
-}
-build();
+//   try {
+//     await esbuild.build({
+//       entryPoints: ["src/js/controller.js"],
+//       outdir: outDir,
+//       bundle: true,
+//       minify: true,
+//       platform: "browser",
+//       format: "esm",
+//       splitting: true,
+//     });
+//     console.log("Build successful!");
+//   } catch (error) {
+//     console.error("Build failed:", error);
+//   }
+// }
+// build();
